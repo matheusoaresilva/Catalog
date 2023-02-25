eda1d9834224ebb66ae1a056eb8c72fb496e5929
Matheus Soares <matheusmatias2622@gmail.com>
2023-02-25 15:14:02 -0300

    H2 database

--- backend/src/main/java/com/matheus/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/matheus/catalog/resources/CategoryResource.java
@@ -3,23 +3,26 @@ package com.matheus.catalog.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.catalog.entities.Category;
+import com.matheus.catalog.services.CategoryService;
 
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		List<Category> list = service.findAll();
 		
 		return ResponseEntity.ok(list);
 	}
---
+++ backend/src/main/java/com/matheus/catalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.matheus.catalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheus.catalog.entities.Category;
+import com.matheus.catalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	CategoryRepository repository;
+	
+	public List<Category> findAll() { 
+		return repository.findAll();
+	}
+}
