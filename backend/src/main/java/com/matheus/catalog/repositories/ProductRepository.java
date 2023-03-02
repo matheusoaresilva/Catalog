d35a30393c46f9a5acabb41cb698c1c80e2a927a
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-02 15:57:46 -0300

    Created ProductRepositoy

---
+++ backend/src/main/java/com/matheus/catalog/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.matheus.catalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheus.catalog.entities.Category;
+import com.matheus.catalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
+ 
