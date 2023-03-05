b4e0611f9d2002fde60cb4b0ee2b08fcff2c1c4f
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-05 00:44:22 -0300

    Added InjectMocks and Mock in ProductServiceTests

--- backend/src/test/java/com/matheus/catalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/matheus/catalog/services/ProductServiceTests.java
@@ -1,8 +1,17 @@
 package com.matheus.catalog.services;
 
+import com.matheus.catalog.repositories.ProductRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
+
+    @InjectMocks
+    ProductService service;
+
+    @Mock
+    ProductRepository repository;
 }
