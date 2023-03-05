66d27ea5574deb791c81058ff2fab9a2f1e6b8f7
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-04 23:31:45 -0300

    Added setup method and private fields for existing and non-existing IDs

--- backend/src/test/java/com/matheus/catalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/matheus/catalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,8 @@ package com.matheus.catalog.repositories;
 
 import com.matheus.catalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,10 +16,19 @@ public class ProductRepositoryTests {
 
     @Autowired
     ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingId = 1L;
+        nonExistingId = 1000L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
 
-        Long existingId = 1L;
         repository.deleteById(existingId);
 
         Optional<Product> result = repository.findById(existingId);
@@ -28,7 +39,6 @@ public class ProductRepositoryTests {
     @Test
     public void deleteShouldThrowEmptyResultDataAcessExceptionWhenIdDontExists(){
         Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-            Long nonExistingId = 1000L;
             repository.deleteById(nonExistingId);
 
             Optional<Product> result = repository.findById(nonExistingId);
