210136ccb9ffbe603f960e750ca56ba5a2f1bcea
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-05 12:48:01 -0300

    Added mock test for ProductService findAllPaged method

--- backend/src/test/java/com/matheus/catalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/matheus/catalog/services/ProductServiceTests.java
@@ -1,18 +1,25 @@
 package com.matheus.catalog.services;
 
+import com.matheus.catalog.entities.Product;
 import com.matheus.catalog.repositories.ProductRepository;
 import com.matheus.catalog.services.exceptions.DatabaseException;
 import com.matheus.catalog.services.exceptions.ResourceNotFoundException;
+import com.matheus.catalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -26,12 +33,18 @@ public class ProductServiceTests {
     Long existingId;
     Long nonExistingId;
     Long dependentId;
+    PageImpl<Product> page;
+    Product product;
 
     @BeforeEach
     void setUp() throws Exception{
         existingId = 1L;
         nonExistingId = 1000L;
         dependentId = 4L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
+
+        Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
 
         Mockito.doNothing().when(repository).deleteById(existingId);
 
