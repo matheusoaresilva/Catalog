91bbdbccda18db9e1e73b03ca9478be6eb063eec
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-05 01:15:55 -0300

    Added test to delete by id method ProductService class

--- backend/src/test/java/com/matheus/catalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/matheus/catalog/services/ProductServiceTests.java
@@ -1,9 +1,13 @@
 package com.matheus.catalog.services;
 
 import com.matheus.catalog.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -14,4 +18,21 @@ public class ProductServiceTests {
 
     @Mock
     ProductRepository repository;
+
+    Long existingId;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingId = 1L;
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists(){
+
+        Assertions.assertDoesNotThrow(() ->{
+            service.delete(existingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
 }
