7f34de6f3ebb62dd33d2ce35db5c39ed8b398e4e
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-07 11:55:49 -0300

    Added test to findAll Page in ProductResource class

--- backend/src/test/java/com/matheus/catalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/matheus/catalog/resources/ProductResourceTests.java
@@ -4,6 +4,7 @@ import com.matheus.catalog.dto.ProductDTO;
 import com.matheus.catalog.services.ProductService;
 import com.matheus.catalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,6 +15,8 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
@@ -33,6 +36,11 @@ public class ProductResourceTests {
         page = new PageImpl<>(List.of(productDTO));
 
         when(service.findAllPaged(any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception{
+        mockMvc.perform(get("/products")).andExpect(status().isOk());
     }
 }
 
