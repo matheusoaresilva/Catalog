238c3deb5406fda9ef6bd4559fc704647ad18463
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-07 11:50:37 -0300

    Added BeforeEach

--- backend/src/test/java/com/matheus/catalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/matheus/catalog/resources/ProductResourceTests.java
@@ -2,6 +2,7 @@ package com.matheus.catalog.resources;
 
 import com.matheus.catalog.dto.ProductDTO;
 import com.matheus.catalog.services.ProductService;
+import com.matheus.catalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(ProductResource.class)
@@ -20,11 +24,15 @@ public class ProductResourceTests {
     @MockBean
     ProductService service;
 
+    ProductDTO productDTO;
     PageImpl<ProductDTO> page;
 
     @BeforeEach
     void setUp() throws Exception {
-        when(service.findAllPaged(null)).thenReturn(page);
-    }
+        productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+
+        when(service.findAllPaged(any())).thenReturn(page);
     }
 }
+
