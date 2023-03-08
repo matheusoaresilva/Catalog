42f2d4f7398a10d1b789503cabcadc3401b2941f
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-08 13:30:22 -0300

    declaring variables

--- backend/src/test/java/com/matheus/catalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/matheus/catalog/services/ProductServiceIT.java
@@ -1,4 +1,20 @@
 package com.matheus.catalog.services;
 
+import com.matheus.catalog.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ProductServiceIT {
+
+    @Autowired
+    ProductService service;
+
+    @Autowired
+    ProductRepository repository;
+
+    Long existingId;
+    Long nonExistingId;
+    Long countTotalProducts;
+
 }
