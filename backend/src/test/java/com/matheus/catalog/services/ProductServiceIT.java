04ce68bcf85c97826d0826aa7569d10b82b2303d
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-08 13:58:33 -0300

    Added Transactional in ProductServiceIT class

--- backend/src/test/java/com/matheus/catalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/matheus/catalog/services/ProductServiceIT.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
