45895a423cee4c1cb4d2ac758a99ccd29ed0a4ee
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-20 07:09:05 -0300

    Create UserInsertValidator.java

---
+++ backend/src/main/java/com/matheus/catalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,41 @@
+package com.matheus.catalog.services.validation;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.matheus.catalog.dto.UserInsertDTO;
+import com.matheus.catalog.entities.User;
+import com.matheus.catalog.repositories.UserRepository;
+import com.matheus.catalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = repository.findByEmail(dto.getEmail());
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
+
